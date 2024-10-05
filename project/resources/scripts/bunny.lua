ec0242ea22ea00fe950753a2ab8f432aaa8793ca
thomz12 <thomz12@hotmail.nl>
2024-10-05 17:24:18 +0900

    Basic bunny spawning, better circle calculation

--- project/resources/scripts/bunny.lua
+++ project/resources/scripts/bunny.lua
@@ -1,7 +1,38 @@
 local time = 0
 
-function update(delta_time)
-    time = time + delta_time
+local bunny_data = {
+    [0] = {
+        hop_distance = 8.0,
+        hop_height = 8.0,
+        hop_duration = 0.5,
+        hop_wait_min = 1.0,
+        hop_wait_max = 2.5,
+    },
+    [1] = {
+        hop_distance = 8.0,
+        hop_height = 16.0,
+        hop_duration = 0.5,
+        hop_wait_min = 1.0,
+        hop_wait_max = 2.0,
+    }
+}
 
-    entity.physics.velocity = juice.vec2.new(0.5, 0)
+local bunny = {}
+
+function start()
+
+    bunny = bunny_data[math.random(0, #bunny_data)]
+
+    juice.routine.create(function()
+        while true do
+            local start_pos = juice.vec2.new(entity.transform.position.x, entity.transform.position.y)
+            juice.routine.wait_seconds_func(bunny.hop_duration, function(x)
+                entity.transform.position = juice.vec3.new(
+                start_pos.x + bunny.hop_distance * x,
+                start_pos.y + bunny.hop_height * math.sin(x * math.pi), 0
+            )
+            end)
+            juice.routine.wait_seconds(math.random() * bunny.hop_wait_max - bunny.hop_wait_min)
+        end
+    end)
 end
